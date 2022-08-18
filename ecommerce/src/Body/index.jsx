341bafd7143c024a9b2673aa0a8a68d666e64fab
Ima <imano.arias@gmail.com>
2022-08-17 21:35:45 -0300

    toma fotitos

---
+++ ecommerce/src/Body/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img1 from '../images/image-product-1.jpg'
+
+const index = () => {
+  return (
+    <>
+    <img src={img1} alt="" />
+    </>
+  )
+}
+
+export default index
